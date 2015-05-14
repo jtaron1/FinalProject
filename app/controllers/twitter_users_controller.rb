4af555a67f781045961a5539b880c9d486b014a8
Greg Friedman <Lifeboat13@gmail.com>
2015-05-14 01:59:00 -0400

    Displays how many retweets for each tweet

--- app/controllers/twitter_users_controller.rb
+++ app/controllers/twitter_users_controller.rb
@@ -20,7 +20,7 @@ class TwitterUsersController < ApplicationController
      twitter_users.each do |twitter_user|
 
       @client.user_timeline(twitter_user.twitter_id)[0..10].each  do |tweet|
-        @all_tweets.push(id:twitter_user.twitter_id, tweet:tweet.full_text, fav_count:tweet.favorite_count)
+        @all_tweets.push(id:twitter_user.twitter_id, tweet:tweet.full_text, fav_count:tweet.favorite_count, rt_count:tweet.retweet_count)
       end
 
      end
